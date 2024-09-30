47f07549dc3919bd362a6c128cc37fa074a66574
shivamgupta02 <shivam.g@taltrip.com>
2024-09-30 12:48:42 +0530

    Add project files.

---
+++ Controllers/GuestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.Controllers
+{
+    public class GuestController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Carrer()
+        {
+            return View();
+        }
+        public IActionResult Contactus()
+        {
+            return View();
+        }
+    }
+}
